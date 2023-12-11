288f92fc20290555e3e99aadd724e7eb074fa95a
Carlos Vinicius <108277450+cviniciusdev@users.noreply.github.com>
2023-12-11 12:12:15 -0300

    Create teste.php

---
+++ TesteGit/teste.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste Git (PULL)</title>
+</head>
+<body>
+  
+    <h1>Título PULL</h1>
+    <p>Criação teste do parágrafo no Documento</p>
+    <?php echo "Novo arquivo criado para aprender PULL"?>
+
+</body>
+</html>
